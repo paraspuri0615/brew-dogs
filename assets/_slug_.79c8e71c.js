0daefeb770d36c48b8f409d75e105cfe8d05a73d
paraspuri0615 <paras.puri0615@gmail.com>
2023-08-14 22:51:05 -0400

    Updates

---
+++ assets/_slug_.79c8e71c.js
@@ -0,0 +1,1 @@
+import{u as e}from"./useAppRoutes.3e96485c.js";import{d as s,n as t}from"./entry.613d057d.js";const m=s({__name:"[slug]",setup(p){const{appRoutes:o}=e();return t(o.home),()=>{}}});export{m as default};
