afc0eb8968dba60d12b0bb5b0abca2449c5339da
paraspuri0615 <paras.puri0615@gmail.com>
2023-08-14 22:42:22 -0400

    Updates

--- _nuxt/_slug_.2ec3b269.js
+++
@@ -1,1 +0,0 @@
-import{u as e}from"./useAppRoutes.3e96485c.js";import{d as s,n as t}from"./entry.f024a0a7.js";const m=s({__name:"[slug]",setup(p){const{appRoutes:o}=e();return t(o.home),()=>{}}});export{m as default};
